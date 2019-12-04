85593a61c301eb1e451addc172066d0d0ca5fe55
brendonpm <brendon.pm@gmail.com>
2019-12-04 16:39:18 -0300

    commit cadastrar veiculos

--- ProjetoIntegradorApp/app/src/main/java/com/senac/go/views/ListaVeiculos.java
+++ ProjetoIntegradorApp/app/src/main/java/com/senac/go/views/ListaVeiculos.java
@@ -2,8 +2,12 @@ package com.senac.go.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.senac.go.R;
 
 public class ListaVeiculos extends AppCompatActivity {
@@ -11,6 +15,21 @@ public class ListaVeiculos extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_lista_veiculos);
+
+        FloatingActionButton bPlusVeic = findViewById(R.id.bPlusVeic);
+
+        bPlusVeic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(ListaVeiculos.this, CadastroVeiculo.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
--- ProjetoIntegradorApp/app/src/main/java/com/senac/go/views/Menu.java
+++ ProjetoIntegradorApp/app/src/main/java/com/senac/go/views/Menu.java
@@ -2,7 +2,10 @@ package com.senac.go.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.senac.go.R;
 
@@ -13,5 +16,54 @@ public class Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_menu);
+
+        Button bListVeic = findViewById(R.id.bListVeic);
+        Button bCadasVeic = findViewById(R.id.bCadasVeic);
+        Button bListAbast = findViewById(R.id.bListAbast);
+        Button bCadasAbast = findViewById(R.id.bCadasAbast);
+        Button bRelatorios = findViewById(R.id.bRelatorios);
+
+        bListVeic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLintVei = new Intent(Menu.this, ListaVeiculos.class);
+                startActivity(intentLintVei);
+            }
+        });
+        bCadasVeic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCadasVei = new Intent(Menu.this, CadastroVeiculo.class);
+                startActivity(intentCadasVei);
+            }
+        });
+        bListAbast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentListAbas = new Intent(Menu.this, ListaAbastecimento.class);
+                startActivity(intentListAbas);
+            }
+        });
+        bCadasAbast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCadasAbas = new Intent(Menu.this, CadastroAbastecimento.class);
+                startActivity(intentCadasAbas);
+            }
+        });
+        bRelatorios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentRela = new Intent(Menu.this, Relatorios.class);
+                startActivity(intentRela);
+            }
+        });
+
+
     }
+
+
+
+
+
 }
