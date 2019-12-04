78abb5a9967e6db71958172ee89c03b0a691e6bc
brendonpm <brendon.pm@gmail.com>
2019-12-04 10:46:36 -0300

    tela menu

--- ProjetoIntegradorApp/app/src/main/java/com/senac/go/views/Loguin.java
+++ ProjetoIntegradorApp/app/src/main/java/com/senac/go/views/Loguin.java
@@ -2,7 +2,10 @@ package com.senac.go.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.senac.go.R;
 
@@ -14,7 +17,16 @@ public class Loguin extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_loguin);
 
+        Button bEntrar = findViewById(R.id.bEntrar);
 
+        bEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent inteMenu = new Intent(Loguin.this, Menu.class);
+                startActivity(inteMenu);
+            }
+        });
 
 
     }
