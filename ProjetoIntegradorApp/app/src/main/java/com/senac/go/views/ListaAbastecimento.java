4ef963f7ecd67111dae8375a046ba88226bb3d93
brendonpm <brendon.pm@gmail.com>
2019-12-04 16:56:18 -0300

    commit cadastrar veiculos

--- ProjetoIntegradorApp/app/src/main/java/com/senac/go/views/ListaAbastecimento.java
+++ ProjetoIntegradorApp/app/src/main/java/com/senac/go/views/ListaAbastecimento.java
@@ -2,8 +2,11 @@ package com.senac.go.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.senac.go.R;
 
 public class ListaAbastecimento extends AppCompatActivity {
@@ -11,6 +14,17 @@ public class ListaAbastecimento extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_lista_abastecimento);
+
+        FloatingActionButton bPlusAbas = findViewById(R.id.bPlusAbas);
+
+        bPlusAbas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(ListaAbastecimento.this, CadastroAbastecimento.class);
+                startActivity(intent);
+            }
+        });
     }
 }
